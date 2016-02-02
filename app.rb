150e86c29eed6ece220e6f2f4a2e59d04cad0c03
Alex Avlonitis <avlonitis@msn.com>
2016-02-02 14:05:58 +0000

    chapter: 19

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
-post '/names' do
-@player1 = params[:Player1]
-@player2 = params[:Player2]
-erb :play
-end
+
+  post '/names' do
+    session['Player1'] = params[:Player1]
+    session['Player2'] = params[:Player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session['Player1']
+    @player2 = session['Player2']
+    erb :play
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
