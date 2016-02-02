0b0118c63cb493b7155171e118f85bbeca1755ab
Constantin <kostiantynkalinin@gmail.com>
2016-02-02 14:23:43 +0000

    Challenge:20. Created progress bar

--- spec/features/User_stories_spec.rb
+++ spec/features/User_stories_spec.rb
@@ -7,5 +7,10 @@ feature 'Adding names' do
     click_button('Submit')
     expect(page).to have_content('Player1 TestName Player2 Testname2')
   end
-
+end
+feature 'Health bar' do
+  scenario 'Player 2 has hit point bar' do
+    visit('http://localhost:4567/play')
+    expect(find('progress')['max']).to eq('100')
+  end
 end
