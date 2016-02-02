2339ac406ba554664b35604a2291f79dfc7cc6f9
Henry Gordon <hwgordon247@gmail.com>
2016-02-02 15:58:17 +0000

    adding of a helper spec

--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'view hit points' do
   scenario 'player 1 viewing player 2' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Mittens HP: 100'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 
 ENV['RACK_ENV'] = 'test'
 
+require 'web_helpers'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
