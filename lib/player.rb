75259754d0f24a1b64eba09e27ecf1eb00c98798
Mic <mic.cassano@gmail.com>
2016-02-04 10:50:03 +0000

    Add additional test on player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,7 @@
 class Player
-  DEFAULT_HIT_POINTS = 10
+  DEFAULT_HIT_POINTS = 100
   attr_reader :name, :hit_points
-  
+
   def initialize(name, hit_points = DEFAULT_HIT_POINTS)
     @name       = name
     @hit_points = hit_points
@@ -12,6 +12,6 @@ class Player
   end
 
   def receive_damage
-    @hit_points -= 1
+    @hit_points -= 10
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,18 +1,27 @@
 require 'player'
 
 describe Player do
-  subject(:sarah) { Player.new("Sarah") }
+  subject(:player1) { Player.new("Ash") }
+  let(:player2)  { double :player }
 
   describe '#name' do
     it "returns the player's name" do
-      expect(sarah.name).to eq "Sarah"
+      expect(player1.name).to eq "Ash"
+    end
+  end
+
+  describe '#attack' do
+    it 'attacks player 2' do
+      expect(player2).to receive(:receive_damage)
+      player1.attack(player2)
     end
   end
 
   describe '#reduce' do
-    it "reduces HP by 1" do
-      expect { sarah.receive_damage }.to change { sarah.hit_points }.by(-1)
+    it "reduces HP by 10" do
+      expect do
+        player1.receive_damage
+      end.to change { player1.hit_points }.by(-10)
     end
   end
-
 end
