b3b84ed4abb9154b9eddc76ae3942f8830331274
Constantin <kostiantynkalinin@gmail.com>
2016-02-04 13:34:07 +0000

    Challenge 26: Unfinished

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,13 @@
-
 class Game
+attr_reader :player1, :player2,
 
-  def attack(player)
-    player.receive_damage
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def attack(player1)
+    @player2.receive_damage
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,24 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  subject(:sarah) { Player.new("sarah") }
-  subject(:mic) { Player.new("mic") }
+  subject(:game) { described_class.new(player1, player2) }
+  # subject(:sarah) { Player.new("sarah") }
+  # subject(:mic) { Player.new("mic") }
+let(:player1) {double (:player)}
+let(:player2) {double (:player)}
 
+  it 'starts with player1' do
+    expect(game.player1).to eq(player1)
+  end
+
+  it 'starts with player2' do
+    expect(game.player2).to eq(player2)
+  end
 
   describe 'attack' do
     it 'damages the player' do
-      expect(mic).to receive(:receive_damage)
-      game.attack(mic)
+      expect(player2).to receive(:receive_damage)
+      game.attack(player1)
     end
   end
 
